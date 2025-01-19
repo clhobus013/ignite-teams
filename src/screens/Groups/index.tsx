67dfcad2ef8be81fbc3f91be6a7e1a4b3d19ea58
claudia <clauu013@gmail.com>
2025-01-19 17:51:15 -0300

    Add styled-components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Container, Title } from './styles';
 
 export function Groups() {
   return (
-    <View>
-        <Text>asd</Text>
-    </View>
+    <Container>
+      <Title>Groups</Title>
+    </Container>
   );
 }
