01c1947daf206e25ab4f13298f2e57d88d6f957d
claudia <clauu013@gmail.com>
2025-01-20 11:23:33 -0300

    configura tema com styled components

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,13 +1,9 @@
 import styled from 'styled-components/native'
+import { DefaultTheme } from 'styled-components';
 
 export const Container = styled.View`
     flex: 1;
-    background-color: #000;
+    background-color: ${({ theme }: DefaultTheme) => theme.COLORS.GRAY_600};
     align-items: center;
     justify-content: center;
-`;
-
-export const Title = styled.Text`
-    color: #fff;
-    font-size: 32;
 `;
