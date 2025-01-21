e92185e785f3c3a0ccb180bcceb342494ff88a6c
claudia <clauu013@gmail.com>
2025-01-21 15:40:23 -0300

    Troca versao biblioteca styled-components

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components/native'
-import { DefaultTheme } from 'styled-components';
 
 export const Container = styled.View`
     flex: 1;
+    background-color: ${({ theme }) => theme.COLORS.GRAY_600};
     background-color: ${({ theme }: DefaultTheme) => theme.COLORS.GRAY_600};
     align-items: center;
     justify-content: center;
