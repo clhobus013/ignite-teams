014eb76d07e39795695a8bd577d997fbc7f1a787
claudia <clauu013@gmail.com>
2025-01-27 11:38:44 -0300

    Add component ButtonIcon

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,7 +1,10 @@
+import { Container, Form } from "./styles";
+
+import { Input } from "@components/Input";
 import { Header } from "@components/Header";
-import { Container } from "./styles";
-import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Highlight } from "@components/Highlight";
+import { ButtonIcon } from "@components/ButtonIcon";
 
 export function Players() {
     return(
@@ -11,6 +14,17 @@ export function Players() {
                 title="Nome da turma"
                 subtitle="Adicione a galera e separe os times"
             />
+
+            <Form>
+                <Input
+                    placeholder="Nome do participante"
+                    autoCorrect={false}
+                />
+
+                <ButtonIcon icon="add" type="PRIMARY"/>
+            </Form>
+
+
             <Button
                 title="Remover turma"
                 type="SECONDARY"
