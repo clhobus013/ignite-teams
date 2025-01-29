3d0706ab3c97819b5d251d936ce0aaeac48e6310
claudia <clauu013@gmail.com>
2025-01-29 19:10:21 -0300

    Add lista de filtros

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 
 import { Input } from "@components/Input";
 import { Header } from "@components/Header";
@@ -6,8 +6,13 @@ import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Filter } from "@components/Filter";
+import { FlatList } from "react-native";
+import { useState } from "react";
 
 export function Players() {
+    const [team, setTeam] = useState('time a')
+    const [players, setPLayers] = useState([])
+
     return(
         <Container>
             <Header showBackButton/>
@@ -25,7 +30,23 @@ export function Players() {
                 <ButtonIcon icon="add" type="PRIMARY"/>
             </Form>
 
-            <Filter title="time a"/>
+            <HeaderList>
+                <FlatList
+                    data={['time a', 'time b']}
+                    keyExtractor={(item)=> item}
+                    renderItem={({item}) => (
+                        <Filter 
+                            title={item}
+                            isActive={team === item}
+                            onPress={() => setTeam(item)}
+                        />
+                    )}
+                    horizontal
+                />
+
+                <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
+            </HeaderList>
+
 
             <Button
                 title="Remover turma"
